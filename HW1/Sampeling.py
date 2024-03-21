68ad5d63b403d648f0ad02a22eb3b4e0576ac0d7
Ali <ali.ali.samadifard@gmail.com>
2024-03-21 09:30:25 +0330

    Add random k fold sampling to smapling.py

--- HW1/Sampeling.py
+++ HW1/Sampeling.py
@@ -17,3 +17,21 @@ def random_sample(df: pd.core.frame.DataFrame, fraction: float):
     sample_df.to_csv(f"F:\AI_IUST\Samples\{file_name}\TRAIN.csv", index=False)
     remaining_df = df.drop(sample_df.index)
     remaining_df.to_csv(f"F:\AI_IUST\Samples\{file_name}\TEST.csv", index=False)
+
+
+def k_fold_random(df: pd.core.frame.DataFrame, k):
+    hash_object = hashlib.sha256(dt.now().strftime("%Y-%m-%d %H:%M:%S").encode())
+    file_name = hash_object.hexdigest()[:8]
+    file_name = "K_FOLD_RANDOM_ " + file_name
+    os.mkdir(f"F:\AI_IUST\Samples\{file_name}")
+    train_set_sz = len(df) // k
+    for i in range(k):
+        if i == k - 1:
+            sample_df.to_csv(f"F:\AI_IUST\Samples\{file_name}\TEST.csv", index=False)
+            break
+        # df_temp = pd.read_csv((f"F:\AI_IUST\Samples\{file_name}\TRAIN_#{i}.csv")
+        sample_df = df.sample(n=train_set_sz)
+        sample_df.to_csv(f"F:\AI_IUST\Samples\{file_name}\TRAIN_#{i+1}.csv", index=False)
+        remaining_df = df.drop(sample_df.index)
+        df = remaining_df
+
